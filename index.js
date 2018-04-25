cd42ec0830bdcea67e2ffdf9961253d09c59cf37
Peter Wang <happypeter@users.noreply.github.com>
2018-04-25 20:16:26 +0800

    const rocks

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(morgan('tiny'))
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost:27017/dbname')
-var db = mongoose.connection
+const db = mongoose.connection
 db.on('error', () => console.error('Mongo Failed to Connect!!!!'))
 db.on('connected', () => console.log('Mongo Connected'))
 // mongoose END
